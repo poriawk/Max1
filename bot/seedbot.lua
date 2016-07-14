6a0bd655880d79cab85008b603f104b4332336dc
poriawk <stouck@yahoo.com>
2016-07-14 13:48:38 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {(173008198)},--Sudo users
+    sudo_users = {92007662},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text  = [[
